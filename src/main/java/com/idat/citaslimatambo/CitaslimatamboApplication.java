7effffff6809f90babbe7948518d48ad9810a794
Juana Custodio <custodim.juana@gmail.com>
2020-01-24 16:42:04 -0500

    first commit

---
+++ src/main/java/com/idat/citaslimatambo/CitaslimatamboApplication.java
@@ -0,0 +1,13 @@
+package com.idat.citaslimatambo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitaslimatamboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitaslimatamboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/citaslimatambo/CitaslimatamboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.citaslimatambo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitaslimatamboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
